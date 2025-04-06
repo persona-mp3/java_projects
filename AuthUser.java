ae64f137c2fb4e1e6876a752292af4a0c23a4c7c
nninfatuation <aiwuyoeghosa@outlook.com>
2025-04-06 19:58:10 +0100

    edits

--- AuthUser.java
+++ AuthUser.java
@@ -13,7 +13,16 @@ public class AuthUser {
     System.out.print("Enter your password: ");
 
     String userPassword = scanner.next();
-    
+   
+    // in java, the comparison operator ie !=, and == only work for primitives
+    // for other datatypes these operators simply check their memory address
+    // in the stack heap. thereby always return false depending on the scenario
+    // if (String a != String b) will return TRUE 
+    // String a == String b will return FALSE 
+    // BECAUSE IT IS COMPARING THE MEMORY ADDRESS IN HEAP MEMORY
+    //
+    // however int a == int b assuming they are the same values will be true;
+
     if (!userPassword.equals(password)) {
       System.out.println("Incorrect Password");
     } else {
